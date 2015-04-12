25d4f24cf8ca096dddf3a14edf2ae97cae61b1da
Too Lee <hojotoolee2@gmail.com>
2015-04-11 22:52:26 -0400

    update run.m

--- run.m
+++ run.m
@@ -1,18 +1,19 @@
 function run
-for i = 1:10
-clear map; close all;
-r = 50;
-c = 50;
-po = 0.0;
-map = random_gen_map(r,c,po);
-tic
-jumppointsearch(map);
-toc
-%saveas(gcf,sprintf('r%dc%dpo%0.2fi%d.jpg',r,c,po,i),'jpg');
-%print(gcf,'my-plot','-dpng');
-%close all;
-tic
-%astar(map);
-toc
-pause(0.1);
+for i = 1:100
+  clear map; close all;
+  r = 5;
+  c = 5;
+  po = 0.40;
+  map = random_gen_map(r,c,po);
+  tic
+  jumppointsearch(map);
+  toc
+  saveas(gcf,sprintf('r%dc%dpo%0.2fi%d.jpg',r,c,po,i),'jpg');
+  save(sprintf('map-r%dc%dpo%0.2fi%d',r,c,po,i),'map');
+  %print(gcf,'my-plot','-dpng');
+  %close all;
+  %tic
+  %astar(map);
+  %toc
+  %pause(0.1);
 end
