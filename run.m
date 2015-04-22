bd66646de4f0002f918fa4ced90c2ec899791bd1
Too Lee <hojotoolee2@gmail.com>
2015-04-21 23:57:55 -0400

    more evaluation with plotting

--- run.m
+++ run.m
@@ -1,4 +1,7 @@
 function run
+clear all; close all;
+
+ITERATIONS = 15;
 
 map_size(1).r = 10;
 map_size(1).c = 10;
@@ -19,35 +22,53 @@ obstacle_percentage = [0.1 0.2 0.3 0.4 0.5 0.6 0.7];
 
 
 for i = 1:size(map_size,2)
+  H = figure; hold on;
   r = map_size(i).r;
   c = map_size(i).c;
   for j = 1:size(obstacle_percentage,2)
     op = obstacle_percentage(j);
-
-    data(j).po = op;
     
-    for it = 1:100
-      clear map; close all;
+    data(i,j).op = op;
+    
+    for it = 1:ITERATIONS
+      clear map; %close all;
       
       map = random_gen_map(r,c,op);
       
       tic
       jumppointsearch(map);
-      data(j).etime_jps(it) = toc;
+      data(i,j).etime_jps(it) = toc;
+      close;
       
-      %saveas(gcf,sprintf('r%dc%dpo%0.2fi%d.jpg',r,c,po,i),'jpg');
-      %save(sprintf('map-r%dc%dpo%0.2fi%d',r,c,po,i),'map');
+      %saveas(gcf,sprintf('r%dc%dpo%0.2fi%d.jpg',r,c,op,i),'jpg');
+      %save(sprintf('map-r%dc%dpo%0.2fi%d',r,c,op,i),'map');
       
       %print(gcf,'my-plot','-dpng');
       %close all;
       tic
       astar(map);
-      data(j).etime_as(it) = toc;
-      pause(0.1);
+      data(i,j).etime_as(it) = toc;
+      close;
+      %       pause(0.1);
     end
+    
+    % populate the mean and std
+    data(i,j).jps_mean = mean(data(i,j).etime_jps);
+    data(i,j).jps_std = std(data(i,j).etime_jps);
+    
+    data(i,j).as_mean = mean(data(i,j).etime_as);
+    data(i,j).as_std = std(data(i,j).etime_as);
+    
+    errorbar(op-0.01,data(i,j).jps_mean,data(i,j).jps_std,'rx');
+    errorbar(op+0.01,data(i,j).as_mean,data(i,j).as_std,'b.');
   end
-  
-  % plot them
-  
-  
+  %set(gca, 'XTick', 1:3, 'XTickLabel', irisSpecies) % Set ticks and tick labels
+  xlabel('Percetage of obstacle in map');
+  ylabel('Time in seconds')
+  title_str = sprintf('Map%dx%d',r,c);
+  title(title_str);
+  legend({'jps', 'astar'}, 'Location', 'Northeast')
+  hold off
+  saveas(H,[title_str '.jpg'],'jpg');
+  close
 end
