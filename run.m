796efaf59141b78f036031f68322c3c4228366f3
Too Lee <hojotoolee2@gmail.com>
2015-04-23 00:22:26 -0400

    adding more plots

--- run.m
+++ run.m
@@ -1,7 +1,7 @@
 function run
 clear all; close all;
-
-ITERATIONS = 15;
+run_str='-2';
+ITERATIONS = 2;
 
 map_size(1).r = 10;
 map_size(1).c = 10;
@@ -9,14 +9,14 @@ map_size(1).c = 10;
 map_size(2).r = 15;
 map_size(2).c = 15;
 
-map_size(3).r = 20;
-map_size(3).c = 20;
+map_size(3).r = 7;
+map_size(3).c = 7;
 
-map_size(4).r = 25;
-map_size(4).c = 25;
+map_size(4).r = 6;
+map_size(4).c = 6;
 
-map_size(5).r = 30;
-map_size(5).c = 30;
+map_size(5).r = 5;
+map_size(5).c = 5;
 
 obstacle_percentage = [0.1 0.2 0.3 0.4 0.5 0.6 0.7];
 
@@ -33,10 +33,10 @@ for i = 1:size(map_size,2)
     for it = 1:ITERATIONS
       clear map; %close all;
       
-      map = random_gen_map(r,c,op);
+      [map,data(i,j).dist(it)] = random_gen_map(r,c,op);
       
       tic
-      jumppointsearch(map);
+      data(i,j).jps_nne(it) = jumppointsearch(map);
       data(i,j).etime_jps(it) = toc;
       close;
       
@@ -46,7 +46,7 @@ for i = 1:size(map_size,2)
       %print(gcf,'my-plot','-dpng');
       %close all;
       tic
-      astar(map);
+      data(i,j).as_nne(it) = astar(map);
       data(i,j).etime_as(it) = toc;
       close;
       %       pause(0.1);
@@ -59,6 +59,14 @@ for i = 1:size(map_size,2)
     data(i,j).as_mean = mean(data(i,j).etime_as);
     data(i,j).as_std = std(data(i,j).etime_as);
     
+    data(i,j).jps_mean_nne = mean(data(i,j).jps_nne);
+    data(i,j).as_mean_nne = mean(data(i,j).as_nne);
+    data(i,j).jps_std_nne = std(data(i,j).jps_nne);
+    data(i,j).as_std_nne = std(data(i,j).as_nne);
+    
+    data(i,j).mean_dist = mean(data(i,j).dist);
+    data(i,j).std_dist = std(data(i,j).dist);
+    
     errorbar(op-0.01,data(i,j).jps_mean,data(i,j).jps_std,'rx');
     errorbar(op+0.01,data(i,j).as_mean,data(i,j).as_std,'b.');
   end
@@ -69,6 +77,53 @@ for i = 1:size(map_size,2)
   title(title_str);
   legend({'jps', 'astar'}, 'Location', 'Northeast')
   hold off
-  saveas(H,[title_str '.jpg'],'jpg');
+  %saveas(H,[title_str run_str '.jpg'],'jpg');
   close
 end
+
+for i = 1:size(map_size,2)
+  for j = 1:size(obstacle_percentage,2)
+    jps_mean(i,j) = data(i,j).jps_mean;
+    jps_std(i,j) = data(i,j).jps_std;
+    as_mean(i,j) = data(i,j).as_mean;
+    as_std(i,j) = data(i,j).as_std;
+    jps_mean_nne(i,j) = data(i,j).jps_mean_nne;
+    jps_std_nne(i,j) = data(i,j).jps_std_nne;
+    as_mean_nne(i,j) = data(i,j).as_mean_nne;
+    as_std_nne(i,j) = data(i,j).as_std_nne;
+    mean_dist(i,j) = data(i,j).mean_dist;
+    std_dist(i,j) = data(i,j).std_dist;
+  end
+end
+
+x = obstacle_percentage;
+H = figure; hold on;
+for i = 1:size(map_size,2)
+  for j = 1:size(obstacle_percentage,2)
+    errorbar(x,mean_dist(i,j),std_dist(i,j),'rx');
+  end
+end
+xlabel('Percetage of obstacle in map');
+ylabel('Euclidean Distance')
+title_str = sprintf('Dist%dx%d',r,c);
+title(title_str);
+hold off
+saveas(H,[title_str run_str '.jpg'],'jpg');
+close
+
+H = figure; hold on;
+for i = 1:size(map_size,2)
+  for j = 1:size(obstacle_percentage,2)
+    errorbar(x-0.01,jps_mean_nne(i,j),jps_std_nne(i,j),'rx');
+    errorbar(x+0.01,as_mean_nne(i,j),as_std_nne(i,j),'b.');
+  end
+end
+xlabel('Percetage of obstacle in map');
+ylabel('Num of Nodes Evaluated')
+title_str = sprintf('Nne%dx%d',r,c);
+title(title_str);
+hold off
+saveas(H,[title_str run_str '.jpg'],'jpg');
+close
+
+save all;
